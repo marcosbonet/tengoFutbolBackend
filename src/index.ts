7dfe944d7970816f227e93d68301152190491cb3
marcos <marcos.bonet@live.com.ar>
2022-11-28 20:07:09 +0100

    Initial Commi

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import http from 'http';
+import createDebug from 'debug';
+import { app } from './app.js';
+import { CustomError } from './interfaces/error.js';
+import { dbConnect } from './db.connect.js';
+const debug = createDebug('W8');
+
+const port = process.env.PORT || 3300;
+const server = http.createServer(app);
+server.on('listening', () => {
+    const addr = server.address();
+    if (addr === null) return;
+    let bind: string;
+    if (typeof addr === 'string') {
+        bind = 'pipe ' + addr;
+    } else {
+        bind =
+            addr.address === '::'
+                ? `http://localhost:${addr?.port}`
+                : `port ${addr?.port}`;
+    }
+    debug(`Listening on ${bind}`);
+});
+
+server.on('error', (error: CustomError, response: http.ServerResponse) => {
+    response.statusCode = error?.statusCode;
+    response.statusMessage = error?.statusMessage;
+    response.write(error.message);
+    response.end();
+});
